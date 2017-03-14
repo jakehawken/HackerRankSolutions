7cb3c1eb8071ca16793b91cbab701bcc4e43c57d
jakehawken <jacobdhawken@gmail.com>
2017-03-13 22:37:37 -0700

    Added solution for the Diagonal Difference problem: https://www.hackerrank.com/challenges/diagonal-difference

--- HackerRankTemplate.m
+++ HackerRankTemplate.m
@@ -1,28 +1,35 @@
-//Solution for <#Problem Name#> on Hacker Rank: <#URL#>
+//Solution for "Diagonal Difference" on Hacker Rank: https://www.hackerrank.com/challenges/diagonal-difference
 #import <Foundation/Foundation.h>
-
-@interface Solver : NSObject
-+ (void)findAndPrintSolutionForInput:(NSArray*)input;
-@end
-
-@implementation Solver
-+ (void)findAndPrintSolutionForInput:(NSArray*)input
-{
-  
-  
-  NSString *solution = [NSString stringWithFormat:@"Solution!"];
-  printf("%s", [solution UTF8String]);
-}
-@end
-
 
 int main(int argc, const char * argv[]){
   @autoreleasepool
   {
-    
-    
-    NSArray *input = @[];
-    [Solver findAndPrintSolutionForInput:input];
+    int n;
+      scanf("%i",&n);
+      int a[n][n];
+      for(int a_i = 0; a_i < n; a_i++){
+         for(int a_j = 0; a_j < n; a_j++){
+          scanf("%d",&a[a_i][a_j]);
+         }
+      }
+      
+      int primary = 0;
+      int secondary = 0;
+      
+      int lc = 0;
+      int rc = n-1;
+      int row = 0;
+      
+      while (lc < n) {
+        primary += a[row][lc];
+        secondary += a[row][rc];
+        
+        lc++;
+        rc--;
+        row++;
+      }
+      
+      printf("%i", abs(secondary - primary));
   }
   return 0;
 }
